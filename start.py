02db007ad09295c32838c936305d4b9c1ca9bb18
rjplaton <rjplaton@gmail.com>
2019-06-27 07:11:28 -0700

    removed intro from Player class

--- start.py
+++ start.py
@@ -1,7 +1,4 @@
-
-
-### NOTES ### 
-#room information in a dictionary
+#location information
 locations = [
 {
 'name' : 'Starting Platform',
@@ -10,6 +7,7 @@ locations = [
 """--Option-- north: Ticket booth
 --Option-- south: Go sit on a bench
 --Option-- wait: Wait awhile
+--Option-- bag: Check your bag inventory
 """
 },
 {
@@ -19,38 +17,91 @@ locations = [
 """--Option-- buy: Buy a ticket
 --Option-- south: Starting area towards the bench
 --Option-- wait: Wait awhile
+--Option-- bag: Check your bag inventory
+"""
+},
+{
+'name' : 'Bench',
+'description' : "Nice. There's an open bench you can sit on.",
+'options' : 
+"""--Option-- sit: Walk to the bench and sit on it
+--Option-- north: Head back to the starting area
+--Option-- wait: Wait awhile standing
+--Option-- bag: Check your bag inventory
+"""
+},
+{
+'name' : 'Train Car 1',
+'description' : """You enter train car 1 and you see a sword.
+It beckons you to pick it up.
+You are the chosen one""",
+'options' : 
+"""--Option-- sword: Fulfill your destiny and pick up the sword!
+--Option-- south: Move back to next train down
+--Option-- wait: Wait awhile standing
+--Option-- bag: Check your bag inventory
+"""
+},
+{
+'name' : 'Train Car 2',
+'description' : "You enter train car 2 and it stinks.",
+'options' : 
+"""--Option-- north: Move to the next train up
+--Option-- south: Head back to next train down
+--Option-- wait: Wait awhile standing
+--Option-- bag: Check your bag inventory
+"""
+},
+{
+'name' : 'Train Car 3',
+'description' : "You try to enter train car 3 and squeeze in. It's full so try to move to another car because this is uncomfortable!",
+'options' : 
+"""--Option-- north: Move to the next train up
+--Option-- south: Head back to next train down
+--Option-- wait: Wait awhile standing and being annoyed
+--Option-- bag: Check your bag inventory
+"""
+},
+{
+'name' : 'Train Car 4',
+'description' : "You enter train car 4 and hear screaming from the next car southward. You are very curious!",
+'options' : 
+"""--Option-- north: Move to the next train up
+--Option-- south: Investigate the car southward
+--Option-- wait: Wait awhile standing and ignoring the screams
+--Option-- bag: Check your bag inventory
+"""
+},
+{
+'name' : 'Train Car 5',
+'description' : """You enter train car 5 and immediately see carnage.
+In the middle of the car, the thing is staring at you""",
+'options' : 
+"""--Option-- fight: Fight the monster
+--Option-- north: RUN back to next train up
+--Option-- wait: Wait awhile standing
+--Option-- bag: Check your bag inventory
 """
 },
 ]
 
-#print(locations['starting_platform'])
-
 #inventory information in a dictionary
-player_inventory = ['Sword','Pen']
+player_inventory = []
 
 
-#inventory items can be picked up
-
-#consider functions that kept on asking for an answer until the user supplies a valid answer
-
-#define global variables like if the player has certain items which would initiatlt be set to false
-#OR
-#use class to define a player that has properties
 class Player:
-    def __init__(self, player_name):
+    def __init__(self,player_name):
         self.player_name = player_name
         self.has_ticket = False
         self.has_sword = False
-        self.has_pen = False
-        self.has_zombiearm = False
+        self.has_seen_monster = False
 
-    def intro(self):
-        print('--- Intro ---')
-        print('Welcome to your daily commute.')
-        print('To win the game, get to work safely.')
-        player_name = input('What is your name?')
-        player1 = Player(player_name)
-        print('Hi', player1.player_name)
+
+    #considered @staticmethod
+    # def intro():
+    # 	# Player.intro
+    # 	print('....')
+    # 	return newly_created_player
 
     def check_inventory(self):
     	print('--- | Bag Inventory |---')
@@ -58,45 +109,29 @@ class Player:
     	for items in player_inventory:
     		print('---',i,items)
     		i += 1
-    
+
+#player can pick up a few different items    
     def buy_ticket(self):
     	self.has_ticket = True
     	player_inventory.append('Ticket')
+    	print('Great! You now have a ticket. You can verify that if you type "bag"')
+    	time.sleep(1)
     
-    def pickup_pen(self):
-    	self.has_pen = True
-    	player_inventory.append('Pen')
-
     def pickup_sword(self):
     	self.has_sword = True
     	player_inventory.append('Sword')
-    
-    def pickup_sword(self):
-    	self.has_sword = True
-    	player_inventory.append('Zombie Arm')
-    
+
+
+#since time progressing, we need a function for "waiting"    
     def waiting(self):
         print('--- You decide to wait. If you wanted to move, try typing one of the direction options. ---')
         time.sleep(1)
-        if game_time > 3:
-            print("Unfortunately, you didn't make it to work on time")
-            Player.game_over(self)
+
     def game_over(self):
         print("G A M E  O V E R!")
         exit()
 
 
-#testing if these things work
-player1 = Player('me')
-player1.intro()
-print(player1.has_ticket)
-print(player_inventory)
-player1.buy_ticket()
-print(player1.has_ticket)
-print(player_inventory)
-
-
-#Pseudocode if we want to create a location class
 class Location:
     def check_train_present(self):
         global train_present
@@ -116,8 +151,6 @@ class Location:
     #def description(self):
     	#print(locations[self])
 
-#Location.description('starting_platform')
-
 #player_name = input('What is your name?')
 #print(player_name)
 
@@ -126,15 +159,23 @@ import time
 game_time = 0
 train_present = False
 
+#establishing a "time limit" to the game   
+def time_limit():
+    if game_time == 20:
+        print("Unfortunately, you didn't make it to work on time")
+        Player.game_over(player_name)
+
 def start_platform():
+	global player_name
 	global game_time
 	global train_present
 	game_time += 1
+	time_limit()
 	print('Turn',game_time)
 	print(locations[0]['description'])
 	Location.check_train_present(start_platform)
 	print(locations[0]['options'])
-	choice = input('? ')
+	choice = input('What do you want to do?  ')
 	if choice == 'north':
 		ticket_booth()
 	elif choice == 'south':
@@ -148,11 +189,149 @@ def start_platform():
 		game_time -= 1
 		start_platform()
 	else:
-		Player.waiting(player1)
+		player_name.waiting()
 		start_platform()
 
+def ticket_booth():
+	global game_time
+	global train_present
+	game_time += 1
+	time_limit()
+	print('Turn',game_time)
+	print(locations[1]['description'])
+	Location.check_train_present(ticket_booth)
+	#print the options
+	print(locations[1]['options'])
+	choice = input('What do you want to do?  ')
+	if choice == 'buy':
+		if "Ticket" in player_inventory:
+			print('You already have a ticket.')
+			time.sleep(1)
+			game_time -= 1
+			ticket_booth()
+		else: 
+			Player.buy_ticket(player_name)
+			ticket_booth()
+	elif choice == 'south':
+		start_platform()
+	elif choice == 'west' and train_present:
+		train_car3()
+	elif choice == 'bag':
+		Player.check_inventory(player_inventory)
+		time.sleep(1)
+		#checking your bag shouldn't progress game_time
+		game_time -= 1
+		ticket_booth()
+	else:
+		Player.waiting(player_name)
+		ticket_booth()
 
+def bench():
+	global game_time
+	global train_present
+	game_time += 1
+	time_limit()
+	print('Turn',game_time)
+	print(locations[2]['description'])
+	Location.check_train_present(bench)
+	print(locations[2]['options'])
+	choice = input('What do you want to do?  ')
+	if choice == 'sit':
+		Player.waiting(player_name)
+		bench()
+	elif choice == 'north':
+		start_platform()
+	elif choice == 'west' and train_present:
+		train_car3()
+	elif choice == 'bag':
+		Player.check_inventory(player_inventory)
+		time.sleep(1)
+		#checking your bag shouldn't progress game_time
+		game_time -= 1
+		ticket_booth()
+	else:
+		Player.waiting(player_name)
+		ticket_booth()
 
+def train_car5():
+	global game_time
+	global train_present
+	game_time += 1
+	time_limit()
+	print('Turn',game_time)
+	print(locations[7]['description'])
+	print(locations[7]['options'])
+	choice = input('What do you want to do?  ')
+	if choice == 'fight':
+		if "Sword" in player_inventory:
+			print('You slice the monster in half and become a hero.')
+			print('What a great story to tell coworkers.')
+			print('Y O U  W I N ! ! !')
+		else: 
+			print('You try to punch the monster...')
+			print('The monster is unaffected and claws you to death.')
+			game_over()
+	elif choice == 'north':
+		train_car4()
+	elif choice == 'west' and train_present:
+		win_platform()
+	elif choice == 'bag':
+		Player.check_inventory(player_inventory)
+		time.sleep(1)
+		#checking your bag shouldn't progress game_time
+		game_time -= 1
+		train_car5()
+	else:
+		Player.waiting(player_name)
+		train_car5()
 
+def train_car1():
+	global game_time
+	global train_present
+	game_time += 1
+	time_limit()
+	print('Turn',game_time)
+	print(locations[3]['description'])
+	print(locations[3]['options'])
+	choice = input('What do you want to do?  ')
+	if choice == 'sword':
+		if "Sword" in player_inventory:
+			print('You already have the sword')
+			time.sleep(1)
+			game_time -= 1
+			train_car1()
+		else: 
+			player_name.pickup_sword()
+			train_car1()
+	elif choice == 'south':
+		train_car2()
+	elif choice == 'west' and train_present:
+		win_platform()
+	elif choice == 'bag':
+		Player.check_inventory(player_inventory)
+		time.sleep(1)
+		#checking your bag shouldn't progress game_time
+		game_time -= 1
+		train_car1()
+	else:
+		Player.waiting(player_name)
+		train_car1()
+
+#moved outside of Player class. consider using a "factory function" via staticmethod in Player class in the future
+def intro():
+    print('--- Intro ---')
+    print('Welcome to your daily commute.')
+    print('To win the game, get to work safely.')
+    time.sleep(1)
+    return input('What is your name?')
+
+#returns the name of the player
+player_name = intro()
+
+#creates the player_name object
+player_name = Player(player_name)
 start_platform()
-	
+
+
+
+
